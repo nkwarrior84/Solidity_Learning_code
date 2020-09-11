66cb1be824c705a2d4b25c9a3b0b57cacbc27bf4
Navneet Kumar Chaurasiya <32726683+nkwarrior84@users.noreply.github.com>
2020-09-11 17:21:44 +0530

    Loops in solidity

---
+++ Loops.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.6.8;
+
+contract Loops 
+{
+ 
+    uint256 id = 7;
+    uint256 counter = 0;
+
+    function loops() public 
+    {
+        while (true) 
+        {
+            break;
+            // while function()
+        }
+
+        for (uint256 i = 0; i <= 7; i++) 
+        {
+            //for loop function()
+        }
+
+        do {
+            //do..while loop function
+        } while (true);
+    }
+}
