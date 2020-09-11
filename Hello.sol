0d6ced3d06e1cc3a83193bb0c5d9342ed824a5df
Navneet Kumar Chaurasiya <32726683+nkwarrior84@users.noreply.github.com>
2020-09-11 17:08:49 +0530

    Hello World Welcome to Solidity!

--- Hello.sol
+++ Hello.sol
@@ -4,23 +4,23 @@ pragma solidity ^0.6.8;
 contract Hello 
 {
     //declare a variable of type string
-    string greeting;
+    string welcome;
 
-    //Create a constructor that initializes @greeting variable
+    //Create a constructor that initializes @welcome variable
     constructor() public 
     {
-        greeting = "Hello World!";
+        welcome = "Hello World Welcome to Solidity!";
     }
 
-    //Define a Get Method that returns value of @greeting
+    //Define a Get Method that returns value of @welcome
     function get() public view returns (string memory) 
     {
-        return greeting;
+        return welcome;
     }
 
-    //Define a Set Method that modify/changes value of @greeting
-    function set(string memory _greet) public
+    //Define a Set Method that modify/changes value of @welcome
+    function set(string memory _well) public
     {
-        greeting = _greet;
+        welcome = _well;
     }
 }
