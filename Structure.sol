217a17fde598e9c441f665ee98a9c7cc2d1aa14a
Navneet Kumar Chaurasiya <32726683+nkwarrior84@users.noreply.github.com>
2020-09-11 17:24:57 +0530

    Structure in solidity

---
+++ Structure.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.6.8;
+
+contract Structure
+{
+    uint256 id;
+    string name;
+    uint256 age;
+    uint256 MIN_AGE = 18;
+    uint256 MAX_AGE = 50;
+    uint256 public c = 0;
+
+    function regEmployee() public returns (string memory) 
+    {
+        for (uint256 i = 0; i < 10; i++) 
+        {
+            c = i;
+            if (i == 4) 
+            {
+                continue;
+            }
+        }
+    }
+}
